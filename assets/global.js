4e7dac15bf083bcf9db873601940055ada7ef6e3
Tihomir Pavlovic <tihomir.pavlovic1112@gmail.com>
2023-04-25 11:42:40 +0200

    sliders desktop

--- assets/global.js
+++ assets/global.js
@@ -15,35 +15,70 @@ $(document).ready(function(){
 
     });
 
-    $('.featured-collection-slider').on('init', function(event, slick){
+    $('.featured-collection-slider, .featured-collection-slider-filters').on('init', function(event, slick){
         $('.lazyload--manual', this).removeClass('lazyload--manual').addClass('lazyload');
     });
 
     $('.featured-collection-slider').slick({
-        slidesToShow: 4,
+      slidesToShow: 3.2,
+        infinite:false,
+        nextArrow: '.next-featured',
+        prevArrow: '.previous-featured',
+        responsive: [
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 2.2,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              arrows: false
+            }
+          },
+      ]
+
+    });
+
+    $('.featured-collection-slider-filters').slick({
+      slidesToShow: 3,
         infinite:false,
         arrows: false,
         responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-                centerMode: true,
-              }
-            },
-          ]
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 2,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              arrows: false
+            }
+          },
+      ]
 
     });
 
     $('.gallery-wrapper').slick({
         slidesToShow: 3.2,
         infinite:false,
-        arrows: false,
+        nextArrow: '.next-gallery',
+        prevArrow: '.previous-gallery',
         responsive: [
             {
               breakpoint: 1000,
               settings: {
                 slidesToShow: 2.2,
+                arrows: false
               }
             },
             {
@@ -51,6 +86,7 @@ $(document).ready(function(){
               settings: {
                 slidesToShow: 1,
                 centerMode: true,
+                arrows: false
               }
             },
         ]
